f691dd9b15f1339ce8bdb70fa1f59e76090ec9eb
ketakopter <79285296+ketakopter@users.noreply.github.com>
2021-03-02 15:01:13 +0100

    Update test_hw.py

--- test/test_hw.py
+++ test/test_hw.py
@@ -1,4 +1,9 @@
-import hw
+import sys
+import os
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
+
+import hw.hw
 
 def test_hw():
   assert hw.hw.data_to_str() == '43.5'
